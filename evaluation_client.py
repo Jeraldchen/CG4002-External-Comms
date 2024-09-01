bebb8abc982d870f30906882b8f68d0cac3b1bab
Jerald Chen <jeraldchenjunhan@gmail.com>
2024-09-01 20:37:25 +0800

    Refactor eval client into a class

--- evaluation_client.py
+++ evaluation_client.py
@@ -1,31 +1,22 @@
-import sys
 from socket import *
-import base64
-from Crypto import Random
-from Crypto.Cipher import AES
-from Crypto.Util.Padding import pad
+from encryption import AESEncryption
 
-def encrypt_and_encode_message(message):        
-    secret_key = bytes('i_l0v3_CG4002_:D', 'utf-8')
-    iv = Random.new().read(AES.block_size) # Random IV
-    cipher = AES.new(secret_key, AES.MODE_CBC, iv) # AES CBC mode
-    encrypted_message = message.encode('utf-8') # Encode message to bytes
-    encrypted_message = cipher.encrypt(pad(encrypted_message, AES.block_size)) # Pad message and encrypt
-    encoded_message = base64.b64encode(iv + encrypted_message) # Encode IV + encrypted message to base64
+class EvaluationClient:
+    def __init__(self, server_name, server_port):
+        self.server_name = server_name
+        self.server_port = server_port
+        self.socket = socket(AF_INET, SOCK_STREAM)
 
-    return encoded_message
+    def send_message(self, message):
+        message = AESEncryption.encrypt_and_encode_message(message)
+        self.socket.send(f"{len(message)}_".encode()) # must split as message is already encoded but "len(message)_" is not encoded
+        self.socket.send(message)
 
-serverName = sys.argv[1]
-serverPort = int(sys.argv[2])
-
-clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((serverName, serverPort))
-
-while True:
-    message = "hello"
-    encrypted_message = encrypt_and_encode_message(message)
-    clientSocket.send(f"{len(encrypted_message)}_".encode()) # send in len(encrypted_message)_encrypted_message format
-    clientSocket.send(encrypted_message)
-    received_message = clientSocket.recv(2048)
-    received_message = received_message.decode('utf-8')
-    print(received_message)
+    def hello(self):
+        self.socket.connect((self.server_name, self.server_port))
+        self.send_message("hello")
+        
+    def receive_message(self):
+        received_message = self.socket.recv(2048).decode()
+        received_message = received_message.split("_", 1)[1] # return the data part of the message
+        return received_message
