dd5e5e5ac74fb775c4b35a95ddd93a63e7b3a291
Jerald Chen <jeraldchenjunhan@gmail.com>
2024-09-06 21:14:05 +0800

    Started on relay node codes

---
+++ relay_node/relay_node_client.py
@@ -0,0 +1,20 @@
+from socket import *
+from evaluation_client.encryption import AESEncryption
+
+class RelayNodeClient:
+    def __init__(self, server_name, server_port):
+        self.server_name = server_name
+        self.server_port = server_port
+        self.socket = socket(AF_INET, SOCK_STREAM)
+        self.encryption = AESEncryption()
+
+    def send_message(self, message):
+        message = self.encryption.encrypt_and_encode_message(message)
+        self.socket.send(f"{len(message)}_".encode()) # must split as message is already encoded but "len(message)_" is not encoded
+        self.socket.send(message)
+        
+    def receive_message(self):
+        received_message = self.socket.recv(2048).decode()
+        received_message = received_message.split("_", 1)[1] # return the data part of the message
+        return received_message
+        
